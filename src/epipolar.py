25644133b2113d76b6e369833421dcb47ebeb5ca
Noshita, Koji <noshita@morphometrics.jp>
2022-06-25 08:44:11 +0900

    🔧 MAINTAIN: remove  function from a notebook

--- src/epipolar.py
+++ src/epipolar.py
@@ -47,3 +47,48 @@ def FF_mat(A1, A2, Rt1, Rt2):
     cam_pos2 = np.array([cam_pos2[0], cam_pos2[1], cam_pos2[2], 1])
     epipole1 = np.dot(P1, cam_pos2)
     return epipole1, epipole2, np.dot(SS_mat(epipole2), np.dot(P2, np.linalg.pinv(P1)))
+
+
+def gene(angles):
+    # 正規化
+    B = list(map(lambda y: y - min(angles), angles))
+    return list(map(lambda y: (y - min(B)) / (max(B) - min(B)), B))
+
+
+def epipole_angle(img_num, epipole_dict, cam_list=[]):
+    """エピポールと輪郭上の点を結んだ直線と，x軸のなす角
+    Parameters
+    ========================
+    img_num: int
+    epipole_dict: dict
+    cam_list: list
+
+    Returns
+    ========================
+    angle_list: list
+
+    """
+    cam = cam_list[img_num]
+    cam.img_load()
+    cam.contour_extraction()
+    angle_list = []
+
+    for epi in epipole_dict[img_num]:
+        epi_angle_list = []
+        epi_x = epi[0]
+        epi_y = epi[1]
+        for color in cam.contour_list:
+            color_angle_list = []
+            for contour in color:
+                contour_angle_list = []
+                for cood in contour:
+                    x = cood[0][0]
+                    y = cood[0][1]
+                    tilt = (y - epi_y) / (x - epi_x)
+                    angle = np.arctan(tilt)
+                    contour_angle_list.append(angle)
+                contour_angle_list = gene(contour_angle_list)
+                color_angle_list.append(contour_angle_list)
+            epi_angle_list.append(color_angle_list)
+        angle_list.append(epi_angle_list)
+    return angle_list
